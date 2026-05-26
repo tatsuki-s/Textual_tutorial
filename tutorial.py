7452b0a0352874f7662a4a39895f039f9959d911
tatsuki-s <zekoushn@gmail.com>
2026-05-26 18:03:18 +0900

    基本機能の実装が完了

--- tutorial.py
+++ tutorial.py
@@ -1,23 +1,58 @@
+from time import monotonic
 from textual.app import App, ComposeResult
 from textual.containers import HorizontalGroup, VerticalScroll
+from textual.reactive import reactive
 from textual.widgets import Button, Digits, Footer, Header
 
 class TimeDisplay(Digits):
     """ディスプレイウィジェット"""
+    start_time = reactive(monotonic)
+    time = reactive(0.0)
+    total = reactive(0.0)
+
+    def on_mount(self) -> None:
+        self.update_timer = self.set_interval(1 / 60, self.update_time, pause=True)
+
+    def update_time(self) -> None:
+        self.time = self.total + monotonic() - self.start_time
+
+    def watch_time(self, time: float) -> None:
+        minutes, seconds = divmod(time, 60)
+        hours, minutes = divmod(minutes, 60)
+        self.update(f"{hours:02,.0f}:{minutes:02.0f}:{seconds:05.2f}")
+
+    def start(self) -> None:
+        self.start_time = monotonic()
+        self.update_timer.resume()
+
+    def stop(self) -> None:
+        self.update_timer.pause()
+        self.total += monotonic() - self.start_time
+        self.time = self.total
+
+    def reset(self) -> None:
+        self.total = 0.0
+        self.time = 0.0
 
 class Stopwatch(HorizontalGroup):
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
-        if event.button.id == "start":
+        button_id = event.button.id
+        time_display = self.query_one(TimeDisplay)
+        if button_id == "start":
+            time_display.start()
             self.add_class("started")
-        elif event.button.id == "stop":
+        elif button_id == "stop":
+            time_display.stop()
             self.remove_class("started")
+        elif button_id == "reset":
+            time_display.reset()
 
     def compose(self) -> ComposeResult:
         yield Button("Start", id="start", variant="success")
         yield Button("Stop", id="stop", variant="error")
         yield Button("Reset", id="reset")
-        yield TimeDisplay("00:00:00:00")
+        yield TimeDisplay()
         
 class StopwatchApp(App):
     CSS_PATH = "stopwatch3.tcss"
